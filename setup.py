4dca7cfd58e33e72d35a465f3faaeb921f9ebf42
kashewnuts <kashun.nuts@gmail.com>
2020-07-19 23:28:48 +0900

    support python3.7 after

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Software Development :: Libraries :: Python Modules']
 
 setup(
@@ -20,6 +20,6 @@ setup(
     version='1.0.0',
     url='https://github.com/mirumee/prices',
     packages=['prices'],
-    install_requires=['babel>=2.5.0', 'typing>=3.6.0'],
+    install_requires=['babel>=2.5.0'],
     classifiers=CLASSIFIERS,
     platforms=['any'])
